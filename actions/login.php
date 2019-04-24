5ea59ca4d2e37e0cc3e736685e14d3d5317eddc9
insanz01 <insankamil002@gmail.com>
2019-04-24 23:13:08 +0700

    memperbaiki action pada fitur login

--- actions/login.php
+++ actions/login.php
@@ -1,7 +1,20 @@
 <?php
+include "../database/db.php";
 if (isset($_POST['login'])) {
   $email = $_POST['email'];
   $password = $_POST['password'];
 
-  echo $email . " dan " . $password;
+  $pass = md5($password);
+
+  $q = "select * from users where email='$email' and password='$pass'";
+  $sql = mysqli_query($c, $q);
+  $res = mysqli_fetch_object($sql);
+
+  if ($res->id) {
+    echo 'berhasil login lah yaaa...';
+    header("Refresh:5; url=../index.php", true, 303);
+  } else {
+    echo "masih gagal nich<br>";
+    header("Refresh:5; url=../index.php", true, 303);
+  }
 }
