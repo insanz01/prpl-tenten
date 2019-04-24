5bd058c50f914580acd31d194f9499086c3046c7
insanz01 <insankamil002@gmail.com>
2019-04-24 23:04:26 +0700

    menambahkan action untuk fitur register dan login

--- actions/register.php
+++ actions/register.php
@@ -1,20 +1,30 @@
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
-<?php
 
+<?php
+include '../database/db.php';
 if (isset($_POST['register'])) {
   $first = $_POST['firstname'];
   if ($_POST['lastname']) $last = $_POST['lastname'];
   else $last = "";
   $email = $_POST['email'];
   $password = $_POST['password'];
+  $pass = md5($password);
 
-  echo "Nama Depan : " . $first . "<br>";
-  echo "Nama Belakang : " . $last . "<br>";
-  echo "Email : " . $email . "<br>";
-  echo "Password : " . $password . "<br>";
-  echo "<script>
-          Swal.fire('Yeah!', 'Your account registered!', 'success');
-        </script>";
+  $q = "insert into users values('', '$first', '$last', '$email', '$pass')";
+  if (mysqli_query($c, $q)) {
+    echo 'your account registered';
+    echo "<script>
+            Swal.fire('Yeah!', 'Your account registered!', 'success');
+          </script>";
+
+    header("Refresh:5; url=../index.php", true, 303);
+  } else {
+    echo "<script>
+            Swal.fire('Ouch!', 'Something wrong with your account!', 'error');
+          </script>";
+
+    header("Refresh:5; url=../index.php", true, 303);
+  }
 }
 
 ?>
