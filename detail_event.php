87adfef8f27d0ecd5854642b8eeef940002f9423
insanz01 <insankamil002@gmail.com>
2019-05-01 22:47:02 +0700

    memperbaiki halaman detail event

--- detail_event.php
+++ detail_event.php
@@ -12,70 +12,39 @@ if (isset($_GET['id'])) {
 
 <div class="container py-5">
   <div class="row py-2">
-    <div class="col-lg-8 col-md-8">
+    <div class="col-xl-8 col-md-8 mx-auto">
       <img class="d-block w-100" src="<?= $res->image_url; ?>" alt="">
     </div>
-    <div class="col-lg-4 col-md-4">
-      <div class="row">
-        <div class="col-lg-12 col-md-12 py-5 card">
-          <p class="text-center">
-            <?= $res->title; ?>
-          </p>
-        </div>
-        <div class="col-lg-12 col-md-12 py-5 card my-2">
-          <p class="text-muted">
-            <p><b>Materi Seminar :</b>
-              Human Model of The World
-              Belief System, Value, State & Perception
-              Limiting Belief & Life Traps
-              Meta Model & Meta Program
-              Surface & Deep Structure
-              Suggestion, Visualisasi & Afirmasi
-            </p>
-            <p> <b>Speakers :</b>
-              Antonius Kurniawan
-              (@Daudantonius)
-              Body Language Practitioner
-              NLP Master & Trainer
-              Best Seller Author (10 Books)
-              National Public Speakers
-            </p>
-            <p><b>Investasi & Cara Pembayaran :</b>
-              Pelajar / Mahasiswa : Rp. 150.000
-              Umum : Rp. 175.000
-              Kolektif (3 Orang ) : Rp. 400.000
-              *Kolektif Maks 2 Minggu Sebelum Acara</p>
-            <p><b>Fasilitas Peserta :</b>
-              -Coffee Break + Snack
-              -Seminar Kit
-              -Student Tools (Modul, Worksheet)
-              -Sertifikat dari Psikologid</p>
-            <p><b>Kontak pendaftaran :</b>
-              082210302030 (SMS / WA)
-              psikolog.id (Line)</p>
-            <p><b>Sistem Pembayaran :</b>
-              Via transfer BCA & Mandiri, akan di tukarkan dengan E-Tiket yang pada waktu pelaksanaan akan ditukar dengan seminar Kit dan sertifikat</p>
-          </p>
-        </div>
-      </div>
-    </div>
   </div>
-  <div class="row">
-    <div class="col-lg-5 col-md-6 py-3 card">
-      <p class="text-muted">
+  <div class="row container card shadow-lg rounded py-2">
+    <div class="col-lg-8 col-md-8 py-3">
+      <h1 class="display-5">
+        <?= $res->title; ?>
+      </h1>
+      <hr>
+      <h4 class="m-0 font-weight-bold">
+        Deskripsi
+      </h4>
+      <p class="text-muted lead">
         <?= $res->description; ?>
       </p>
-    </div>
-    <div class="mx-auto"></div>
-    <div class="col-lg-5 col-md-6 py-3 card">
-      <p class="text-muted">
-        <p><b>Date : </b><?= $res->date_e; ?></p>
+      <h4 class="m-0 font-weight-bold">
+        Tanggal :
+      </h4>
+      <p class="text-muted lead">
+        <?= $res->date_e; ?>
       </p>
-      <p class="text-muted">
-        <p><b>Time : </b><?= $res->jam; ?> – Selesai
-          LEARN Coworking Space
-          Ruko Pasar Modern Blok R46
-          Kota Harapan Indah – <?= $res->lokasi; ?></p>
+      <h4 class="m-0 font-weight-bold">
+        Waktu :
+      </h4>
+      <p class="text-muted lead">
+        <?= $res->jam; ?> – <?= $res->jam_akhir; ?>
+      </p>
+      <h4 class="m-0 font-weight-bold">
+        Lokasi :
+      </h4>
+      <p class="text-muted lead">
+        <?= $res->detail_lokasi; ?>, <?= $res->lokasi; ?>
       </p>
     </div>
   </div>
