5c05b0987df13c6b1169b7962c9ee277b5f6b6d7
insanz01 <insankamil002@gmail.com>
2019-05-03 13:25:01 +0700

    menambahkan fitur transaksi

--- actions/login.php
+++ actions/login.php
@@ -11,8 +11,13 @@ if (isset($_POST['email'])) {
   $res = mysqli_fetch_object($sql);
 
   if ($res->id) {
-    echo 'berhasil login lah yaaa...';
-    header("Refresh:5; url=../admin/index.php", true, 303);
+    echo 'berhasil login, tunggu 5 detik!';
+    if ($email == "ftenten302@gmail.com") {
+      header("Refresh:5; url=../admin/index.php", true, 303);
+    } else {
+      $GLOBALS['login'] = true;
+      header("Refresh:5; url=../index.php", true, 303);
+    }
   } else {
     echo "masih gagal nich<br>";
     header("Refresh:5; url=../index.php", true, 303);
--- detail_event.php
+++ detail_event.php
@@ -21,7 +21,7 @@ if (isset($_GET['id'])) {
       <h4 class="float-left font-weight-light">Harga: Rp. <?= $res->harga; ?> ,-</h4>
     </div>
     <div class="col-6">
-      <button class="btn btn-outline-success float-right">
+      <button class="btn btn-outline-success float-right" data-toggle="modal" data-target="#beliModal">
         Daftar Sekarang
       </button>
     </div>
@@ -56,6 +56,42 @@ if (isset($_GET['id'])) {
       <p class="text-muted lead">
         <?= $res->detail_lokasi; ?>, <?= $res->lokasi; ?>
       </p>
+    </div>
+  </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="beliModal" tabindex="-1" role="dialog" aria-labelledby="beliModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title text-white" id="beliModalLabel">Beli Tiket ?</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <form action="actions/beli_tiket.php" method="post">
+        <div class="modal-body">
+          <input type="hidden" name="seminar_id" value="<?= $id; ?>">
+          <div class="form-group">
+            <label for="jumlah" class=" text-white">Jumlah Tiket</label>
+            <select name="jumlah" id="jumlah" class="form-control">
+              <option value="1" selected>1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+            </select>
+          </div>
+          <div class="form-group">
+            <label for="email" class=" text-white">Email</label>
+            <input type="email" class="form-control" name="email" id="email" placeholder="Masukan email anda">
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary text-white" data-dismiss="modal">Close</button>
+          <button type="submit" class="btn btn-primary text-white">Save changes</button>
+        </div>
+      </form>
     </div>
   </div>
 </div>
